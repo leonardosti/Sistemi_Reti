622d03d6e22c90de7d0a472b8e33369a0715c160
Leonardo <leonardo.osti@iisviolamarchesini.edu.it>
2023-10-09 23:23:40 +0200

    Conversioni

--- BinInt/Program.cs
+++ BinInt/Program.cs
@@ -9,6 +9,7 @@ namespace BinInt
             bool[] b = new bool[32];
             int[] dp = new int[4];
             string bit = "";
+            long numero = 0;
             int cnt = 0;
 
             // Conversione binario a intero
@@ -23,6 +24,7 @@ namespace BinInt
                 cnt++;
             }
             Console.WriteLine($"Numero intero: {Convert_Binario_To_Intero(b)}");
+
             // Conversione decimale puntato a intero
             Console.WriteLine("Inserire il numero decimale puntato (max 4 cifre)");
             bit = Console.ReadLine();
@@ -31,7 +33,21 @@ namespace BinInt
             {
                 dp[i] = Convert.ToInt32(vet[i]);
             }
+
             Console.WriteLine($"Numero intero: {Convert_Decimale_Puntato_To_Intero(dp)}");
+
+            Console.WriteLine($"Numero intero: {Convert_Binario_To_Decimale_Puntato(b)}");
+
+            Console.WriteLine($"Numero intero: {Convert_Decimale_Puntato_To_Binario(dp)}");
+
+            Console.WriteLine("Inserire numero");
+            numero = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine($"Numero intero: {Convert_Numero_Decimale_To_Binario(numero)}");
+
+            Console.WriteLine("Inserire numero");
+            numero = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine($"Numero intero: {Convert_Numero_Decimale_To_Decimale_Puntato(numero)}");
+
             Console.ReadLine();
         }
         static int Convert_Binario_To_Intero(bool[] b)
@@ -54,6 +70,83 @@ namespace BinInt
                 potenza++;
             }
             return risultato;
+        }
+        static int[] Convert_Binario_To_Decimale_Puntato(bool[] b)
+        {
+            int[] risultato = new int[4];
+            int indice = b.Length - 1;
+            double n = 0;
+            for (int j = 0; j < 4; j++)
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    if (b[indice])
+                    {
+                        n += (1 * Math.Pow(2, i));
+                        indice--;
+                    }
+                }
+                risultato[j] = Convert.ToInt32(n);
+                n = 0;
+            }
+            return risultato;
+        }
+        static bool[] Convert_Decimale_Puntato_To_Binario(int[] dp)
+        {
+            bool[] risultato = new bool[32];
+            int resto, indice = risultato.Length - 1;
+            for (int i = 0; i < 4; i++)
+            {
+                do
+                {
+                    resto = dp[i] % 2;
+                    if (resto == 1)
+                    {
+                        risultato[indice] = true;
+                    }
+                    indice--;
+                    dp[i] = dp[i] / 2;
+                } while (dp[i] > 0);
+
+            }
+            return risultato;
+        }
+        static bool[] Convert_Numero_Decimale_To_Binario(long numero)
+        {
+            long resto;
+            bool[] risultato = new bool[32];
+            int indice = risultato.Length - 1;
+            do
+            {
+                resto = numero % 2;
+                if (resto == 1)
+                {
+                    risultato[indice] = true;
+                }
+                indice--;
+                numero = numero / 2;
+            } while (numero > 0);
+            return risultato;
+        }
+        static int[] Convert_Numero_Decimale_To_Decimale_Puntato(long numero)
+        {
+            int[] risultato = new int[4];
+            int indice = 0;
+            long resto;
+            do
+            {
+                resto = numero % 256;
+                risultato[indice] = Convert.ToInt32(resto);
+                numero = numero / 256;
+                indice++;
+
+            } while (numero > 0);
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(risultato[i]);
+            }
+            return risultato;
+
         }
     }
 }
